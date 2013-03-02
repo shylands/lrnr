c130375483945443686d2ae75f9598f87bf7e923
Matt Campbell <mwc0000@gmail.com>
2013-03-02 18:15:09 +0000

    Vote

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,10 @@
 class Post < ActiveRecord::Base
-  attr_accessible :title, :topic, :url, :user
+  attr_accessible :title, :topic, :url, :user, :vote
   belongs_to :user
   belongs_to :topic
+  has_many :votes
 
   extend FriendlyId
   friendly_id :title, use: :slugged
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130302125413) do
+ActiveRecord::Schema.define(:version => 20130302173606) do
 
   create_table "posts", :force => true do |t|
     t.string   "title"
@@ -49,5 +49,15 @@ ActiveRecord::Schema.define(:version => 20130302125413) do
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
   add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
+
+  create_table "votes", :force => true do |t|
+    t.integer  "user_id"
+    t.integer  "post_id"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  add_index "votes", ["post_id"], :name => "index_votes_on_post_id"
+  add_index "votes", ["user_id"], :name => "index_votes_on_user_id"
 
 end
