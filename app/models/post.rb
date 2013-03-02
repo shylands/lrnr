a0560f178ceed7cf21bbc990abae442a7f3a574d
John Rowe <john.barr.rowe@me.com>
2013-03-02 21:21:14 +0000

    Initial pass at tags models.

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,7 @@ class Post < ActiveRecord::Base
   belongs_to :user
   belongs_to :topic
   has_many :votes
+  has_many :tags
 
   extend FriendlyId
   friendly_id :title, use: :slugged
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130302195948) do
+ActiveRecord::Schema.define(:version => 20130302210430) do
 
   create_table "bookmarks", :force => true do |t|
     t.integer  "post_id"
@@ -31,6 +31,13 @@ ActiveRecord::Schema.define(:version => 20130302195948) do
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
     t.string   "slug"
+  end
+
+  create_table "tags", :force => true do |t|
+    t.integer  "post_id"
+    t.string   "title"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
   create_table "topics", :force => true do |t|
