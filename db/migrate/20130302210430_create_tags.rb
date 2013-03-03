a52889a88bf21b78d1da2e460fd1e92861b93c1c
Tommy Palmer <hi@tommyp.org>
2013-03-03 13:41:53 +0000

    removing shit tags

--- db/migrate/20130302210430_create_tags.rb
+++
@@ -1,9 +0,0 @@
-class CreateTags < ActiveRecord::Migration
-  def change
-    create_table :tags do |t|
-      t.column :post_id, :integer
-      t.column :title, :string
-      t.timestamps
-    end
-  end
-end
