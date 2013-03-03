3f371c63d6d21a3a1b51a2d2724738539d69f550
Tommy Palmer <hi@tommyp.org>
2013-03-03 12:47:57 +0000

    fixing auth

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,6 +1,6 @@
 class TagsController < ApplicationController
 
-  skip_before_filter :authenticate_user!, :except => [:index, :show]
+  skip_before_filter :authenticate_user!, :only => [:index, :show]
   # GET /tags
   # GET /tags.json
   def index
