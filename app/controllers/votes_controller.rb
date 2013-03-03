8123d08e104240390e2c62f58e6995a96b9dfaf2
David Rice <me@davidjrice.co.uk>
2013-03-03 16:20:35 +0000

    Fix vote count

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -6,6 +6,8 @@ class VotesController < ApplicationController
     @post = Post.find(params[:post_id])
     @vote = Vote.where(post_id: @post.id, user_id: current_user.id).first_or_create
 
+    @count = @post.votes.count
+
     respond_to do |format|
       format.js
     end
@@ -14,6 +16,8 @@ class VotesController < ApplicationController
   def destroy
     @post = Post.find(params[:post_id])
     Vote.where(user_id: current_user.id, post_id: @post.id).destroy_all
+
+    @count = @post.votes.count
 
     respond_to do |format|
       format.js
