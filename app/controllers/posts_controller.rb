8f72132827ccb0ad640ba85274a97c7132028404
David Rice <me@davidjrice.co.uk>
2013-03-03 15:58:29 +0000

    Return posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -29,9 +29,9 @@ class PostsController < ApplicationController
   # GET /search
   # GET /search.json
   def search
-    # @posts = Post.tagged_with(params[:q])
+    # 
   
-
+    @posts = Post.tagged_with(params[:q])
     @tags = Tag.search(params[:q])
 
     # @tags = @posts.map {|p| p.tags }.flatten.map {|t| t.name }.uniq
